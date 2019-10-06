db5e1bf373a9e2775090edb565e3e4b9684759b6
Rodney Barnes <rdnybrns@gmail.com>
2019-10-06 15:54:26 -0400

    Designed the searchbox.

--- polling.js
+++ polling.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     'polling.php',
     { action: 'getPollingStations' },
     function(data) {
-      $('#results').html(data)
+      // $('#results').html(data)
       //let el = $('<div></div>')
       //el.html(data)
       console.log(data.search('<body>'))
@@ -11,4 +11,32 @@ $(document).ready(function() {
     },
     'html'
   )
+
+  /**
+   * The following block is to set the box shadow for the search bar when it is either in focus or hovered over, and
+   * unset it when the focus is blurred or the mouse exits while the input is not in focus.
+   */
+  let isInputFocused = false;
+
+  function setBoxShadow(){
+    $("#searchbar").css("box-shadow", "0 0 3px rgba(0,0,0,0.16), 0 0 3px rgba(0,0,0,0.23)")
+  }
+
+  function unsetBoxShadow(){
+    $("#searchbar").css("box-shadow", "none")
+  }
+
+  $("#searchbar input").focus(function(){
+    isInputFocused = true
+    setBoxShadow()
+  }).blur(function(){
+    unsetBoxShadow()
+    isInputFocused = false
+  })
+
+  $("#searchbar").hover(setBoxShadow, function(){
+    if(isInputFocused === false)
+      unsetBoxShadow()
+  })
+
 })
