0e10c2410843c8997f7e825e7fa51a4ecef4b690
Rodney Barnes <rdnybrns@gmail.com>
2019-10-13 16:14:50 -0400

    Set up the back end to retrieve and return polling station and candidate info.

--- polling.js
+++ polling.js
@@ -1,16 +1,62 @@
 $(document).ready(function() {
-  $.get(
-    'polling.php',
-    { action: 'getPollingStations' },
-    function(data) {
-      // $('#results').html(data)
-      //let el = $('<div></div>')
-      //el.html(data)
-      console.log(data.search('<body>'))
-      console.log(data.search('</body>'))
-    },
-    'html'
-  )
+
+  const key = 'AkpqQWmpsJQMgLXtKOlvLRJa1VQKsvVtQJCbVDHwHfV1Vl05N4wviEjYoKZllQ8g'
+
+  $('#searchBtn').click(getPostalCode)
+
+  function getPollingInfo(postalCode, province){
+    console.log('Sending query...')
+    $.get(
+      'polling.php',
+      { action: 'getPollingInfo', postalCode: postalCode, province: province },
+      function(data) {
+        // $('#results').html(data)
+        //let el = $('<div></div>')
+        //el.html(data)
+        console.log('Received data:')
+        console.log(data)
+      },
+      'html'
+    )
+  }
+
+  /**
+   * Takes the user's input and uses the Bing Maps REST API to translate that input into a postal code
+   * and pass that postal code into the polling.php script, which will use it to retrieve the user's
+   * polling station info.
+   */
+  function getPostalCode(){
+    let url = 'https://dev.virtualearth.net/REST/v1/Locations?countryRegion=CA&addressLine={query}&key={key}'
+    let query = $('#searchInput').val()
+
+    // If the user input was a postal code, we must pass that into the postalCode parameter and not the addressLine parameter
+    // Regex source: https://howtodoinjava.com/regex/canada-postal-code-validation/
+    const postalCodeRegex = new RegExp(/^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$/)
+    if(postalCodeRegex.test(query)){
+      console.log('postal code was passed')
+      query = "&postalCode="+query
+    }
+    url = url.replace('{query}', query).replace('{key}', key)
+
+    // I had trouble accessing the Bing Maps Rest API from a server that used SSL; this blog post helped with that:
+    // https://blogs.bing.com/maps/2015/03/05/accessing-the-bing-maps-rest-services-from-various-javascript-frameworks
+    $.ajax({
+      url: url,
+      datatype: 'jsonp',
+      jsonp: 'jsonp',
+      success: function (r){
+        // console.log(r)
+        const postalCode = r.resourceSets[0].resources[0].address.postalCode.replace(/\s+/g,'')
+        const province = r.resourceSets[0].resources[0].address.adminDistrict
+        getPollingInfo(postalCode, province)
+      },
+      error: function (e) {
+        console.log('Something went wrong: ')
+        console.log(e)
+      }
+    })
+  }
+  
 
   /**
    * The following block is to set the box shadow for the search bar when it is either in focus or hovered over, and
@@ -19,14 +65,14 @@ $(document).ready(function() {
   let isInputFocused = false;
 
   function setBoxShadow(){
-    $("#searchbar").css("box-shadow", "0 0 3px rgba(0,0,0,0.16), 0 0 3px rgba(0,0,0,0.23)")
+    $('#searchbar').css('box-shadow', '0 0 3px rgba(0,0,0,0.16), 0 0 3px rgba(0,0,0,0.23)')
   }
 
   function unsetBoxShadow(){
-    $("#searchbar").css("box-shadow", "none")
+    $('#searchbar').css('box-shadow', 'none')
   }
 
-  $("#searchbar input").focus(function(){
+  $('#searchbar input').focus(function(){
     isInputFocused = true
     setBoxShadow()
   }).blur(function(){
@@ -34,18 +80,14 @@ $(document).ready(function() {
     isInputFocused = false
   })
 
-  $("#searchbar").hover(setBoxShadow, function(){
+  $('#searchbar').hover(setBoxShadow, function(){
     if(isInputFocused === false)
       unsetBoxShadow()
   })
 
-  let map,
-  infobox,
-  userLocationLayer,
-  userPushpinsLayer,
-  schoolsLayer
+  let map
 
-  $("#searchBtn").click(getMap)
+  
 
   function loadMapScenario(){
     getMap()
@@ -57,7 +99,7 @@ $(document).ready(function() {
   function getMap() {
     // Center the map on Hamilton, Ontario with an appropriate zoom level so the user
     // can see the entire city.
-    console.log("loading map")
+    console.log('loading map')
     map = new Microsoft.Maps.Map(document.getElementById('ps-map'), {
       center: new Microsoft.Maps.Location(43.2557, -79.8711),
       zoom: 12
--- polling.php
+++ polling.php
@@ -1,9 +1,53 @@
 <?php
+include 'simple_html_dom.php';
 
-if($_REQUEST['action']){
-    $response = file_get_contents('https://elections.ca/Scripts/vis/voting?L=e&ED=35035&EV=51&EV_TYPE=1&PC=L8L6S9&PROV=ON&PROVID=35&MAPID=&QID=3&PAGEID=31&TPAGEID=&PD=&STAT_CODE_ID=15');
-    // $response = "test";
-    echo $response;
+if($_REQUEST['action'] == 'getPollingInfo'){
+    $postalCode = $_REQUEST['postalCode'];
+    $province = $_REQUEST['province'];
+    $url = 'https://elections.ca/Scripts/vis/voting?L=e&ED=35035&EV=51&EV_TYPE=1&PC={postalcode}&PROV={province}&PROVID=35&MAPID=&QID=3&PAGEID=31&TPAGEID=&PD=&STAT_CODE_ID=15';
+    $url = str_replace('{postalcode}', $postalCode, $url);
+    $url = str_replace('{province}', $province, $url);
+    $html = file_get_html($url);
+    $pollingInfo = array();
+
+    $pollingInfo[] = array('coordinates' => $html->find('input[id=coordinate1]', 0)->value);
+    $pollingStation = array();
+
+    foreach($html->find('div[id=divOrdPoll] ul li') as $info){
+        $pollingStation[] = $info->innertext;
+    }
+    $pollingInfo[] = array('hours' => substr($pollingStation[0], 18, 22));
+    $pollingInfo[] = array('name' => $pollingStation[1]);
+    $pollingInfo[] = array('address' => $pollingStation[2]);
+    $pollingInfo[] = array('city' => $pollingStation[3]);
+    $pollingInfo[] = array('candidates' => getCandidates($postalCode, $province));
+
+    //echo json_encode(getCandidates($postalCode, $province));
+    echo json_encode($pollingInfo);
+}
+
+function getCandidates($postalCode, $province){
+    $url = 'https://elections.ca/Scripts/vis/candidates?L=e&ED=35035&EV=51&EV_TYPE=1&PC={postalCode}&PROV={province}&PROVID=35&QID=-1&PAGEID=17';
+    $url = str_replace('{postalcode}', $postalCode, $url);
+    $url = str_replace('{province}', $province, $url);
+    $html = file_get_html($url);
+    $candidateInfo = array();
+    $candidates = array();
+
+    foreach($html->find('table tr') as $candidate){
+        $candidates[] = $candidate;
+    }
+    array_shift($candidates);   // We don't need the table headers.
+    foreach($candidates as $candidate){
+        $candidateArray = array();
+        $candidateArray[] = array('name' => trim($candidate->find('td', 0)->innertext));
+        $candidateArray[] = array('party' => trim($candidate->find('td', 2)->innertext));
+        $candidateArray[] = array('phone' => trim($candidate->find('td', 3)->innertext));
+        if(isset($candidate->find('td a[href]', 0)->href))
+            $candidateArray[] = array('website' => trim($candidate->find('td a[href]', 0)->href));
+        $candidateInfo[] = $candidateArray;
+    }
+    return $candidateInfo;
 }
 
 
